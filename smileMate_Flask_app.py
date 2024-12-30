74bd018ad4a2242130a38fd1cfd1686fdd1d3e8c
Shelson Myloth Thomas <shelsonmt438@gmail.com>
2024-12-30 08:25:27 -0500

    Add files via upload

---
+++ smileMate_Flask_app.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, Response
+import cv2
+import numpy as np
+from keras.preprocessing import image
+import warnings
+warnings.filterwarnings("ignore")
+from keras.models import load_model
+
+app = Flask(__name__)
+
+# Load the pre-trained model
+model = load_model("best_model.h5")
+
+# Load the Haar cascade for face detection
+face_haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Initialize the webcam
+cap = cv2.VideoCapture(0)
+
+def generate_frames():
+    while True:
+        ret, test_img = cap.read()
+        if not ret:
+            break
+        gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2RGB)
+
+        faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
+
+        for (x, y, w, h) in faces_detected:
+            cv2.rectangle(test_img, (x, y), (x + w, y + h), (255, 0, 0), thickness=7)
+            roi_gray = gray_img[y:y + w, x:x + h]
+            roi_gray = cv2.resize(roi_gray, (224, 224))
+            img_pixels = image.img_to_array(roi_gray)
+            img_pixels = np.expand_dims(img_pixels, axis=0)
+            img_pixels /= 255
+
+            predictions = model.predict(img_pixels)
+
+            # Find the max indexed array
+            max_index = np.argmax(predictions[0])
+
+            emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
+            predicted_emotion = emotions[max_index]
+
+            cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+        resized_img = cv2.resize(test_img, (1000, 700))
+        _, buffer = cv2.imencode('.jpg', resized_img)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
